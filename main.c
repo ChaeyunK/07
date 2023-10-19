4bbd5b957730b1521ec3bbfdee0973cb38b9b18c
ChaeyunK <iehate52@gmail.com>
2023-10-19 15:44:10 +0900

    3

--- main.c
+++ main.c
@@ -4,13 +4,15 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int i;
+	int i=10;
+	printf("before i=%d\n", i);
+	i=inc(i);
+	printf("after i=%d\n", i);
 	
-	for(i=0;i<5;i++)
-	{
-		int temp = 1;
-		printf("temp = %d\n", temp);
-		temp++;
-	}
 	return 0;
+}
+int inc(int counter)
+{
+	counter++;
+	return counter;
 }
