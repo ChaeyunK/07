997e1041c09487ff818eb4dcd306cd99f162802c
ChaeyunK <iehate52@gmail.com>
2023-10-19 15:57:55 +0900

    4

--- main.c
+++ main.c
@@ -2,17 +2,21 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
+void f(void);
 
+int i;
 int main(int argc, char *argv[]) {
-	int i=10;
-	printf("before i=%d\n", i);
-	i=inc(i);
-	printf("after i=%d\n", i);
+	for (i=0; i<5; i++)
+	{
+		f();
 	
+	}	
 	return 0;
 }
-int inc(int counter)
+
+void f(void)
 {
-	counter++;
-	return counter;
+	for(i=0; i<10; i++)
+		printf("#");
 }
+
